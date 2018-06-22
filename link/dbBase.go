b816654fd697e366b878b78c94191784d9ea7e5b
developersPHP <738155996@qq.com>
2018-06-22 19:13:06 +0800

    封装rabbitmq的client

--- link/dbBase.go
+++ link/dbBase.go
@@ -1,5 +1,6 @@
 package link
 
+//TODO 由于这里的rabbit和redis和mysql几个驱client差异比较大，所以先设置一个空接口,以后抽象一层共性可以放这里
 type dbBase interface {
 	//在这里要定义数据库所可以提供的操作方法
 }
--- link/messagelink/rabbit.go
+++ link/messagelink/rabbit.go
@@ -1,1 +1,107 @@
 package messagelink
+
+import (
+	"github.com/streadway/amqp"
+	"github.com/uber/jaeger-client-go/crossdock/log"
+	"sync"
+)
+
+type RabbitMQ struct {
+	conn *amqp.Connection
+	ch   *amqp.Channel
+	q    amqp.Queue
+}
+
+var (
+	once sync.Once
+	obj  *RabbitMQ
+)
+
+//单例模式，减少结构体声明
+func NewRabbitMQFactory(c *amqp.Connection) *RabbitMQ {
+	once.Do(func() {
+		obj = new(RabbitMQ)
+		obj.conn = c
+	})
+	return obj
+}
+func FailError(err error, msg string) {
+	if err != nil {
+		log.Printf("err%s", msg)
+	}
+}
+
+func (this *RabbitMQ) ChoseChannel() *RabbitMQ {
+	ch, err := this.conn.Channel()
+	this.ch = ch
+	FailError(err, "get channel failed")
+	return this
+}
+
+func (this *RabbitMQ) CreateExchange(name string, kind string, durable, autoDelete, internal, noWait bool, arg map[string]interface{}) error {
+	err := this.ch.ExchangeDeclare(
+		name,
+		kind,
+		durable,
+		autoDelete,
+		internal,
+		noWait,
+		arg,
+	)
+	FailError(err, "get channel failed")
+	return err
+}
+
+func (this *RabbitMQ) Publish(exchange, key string, mandatory, immediate bool, contentType string, body []byte) {
+	err := this.ch.Publish(
+		exchange,
+		key, //route_key
+		mandatory,
+		immediate,
+		amqp.Publishing{
+			//DeliveryMode: amqp.Persistent, //设置消息持久化
+			ContentType: contentType,
+			Body:        body,
+		})
+	FailError(err, "Failed to publish a message")
+}
+
+func (this *RabbitMQ) CreateQueue(queueName string, durable, autoDelete, exclusive, noWait bool, args map[string]interface{}) (*RabbitMQ, error) {
+	q, err := this.ch.QueueDeclare(
+		queueName,
+		durable,
+		autoDelete,
+		exclusive,
+		noWait,
+		args,
+	)
+	FailError(err, "Failed to create queue ")
+	this.q = q
+	return this, nil
+}
+
+func (this *RabbitMQ) CreateConsume(customName string, autoAck, exclusive, noLocal, noWait bool, args map[string]interface{}) (<-chan amqp.Delivery, error) {
+	msgs, err := this.ch.Consume(
+		this.q.Name,
+		customName,
+		autoAck,
+		exclusive,
+		noLocal,
+		noWait,
+		args,
+	)
+	FailError(err, "Failed to register a consumer")
+	return msgs, err
+}
+
+func (this *RabbitMQ) ToBind(routeKeys []string, exchange string, noWait bool, args map[string]interface{}) {
+	for _, v := range routeKeys {
+		err := this.ch.QueueBind(
+			this.q.Name, // queue name
+			v,           // binding key
+			"exchange",  // exchange
+			noWait,
+			args)
+		FailError(err, "Failed to bind a queue")
+	}
+}
